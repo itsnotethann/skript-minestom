19115f694899c4e15a64f67898f27e1dd701a257
hapily <87914807+hapily04@users.noreply.github.com>
2026-07-27 21:10:24 -0500

    fix: remove mention of disable and enable in help message

--- minestom/src/main/java/com/github/hapily04/skriptminestom/command/SkriptCommand.java
+++ minestom/src/main/java/com/github/hapily04/skriptminestom/command/SkriptCommand.java
@@ -9,11 +9,13 @@ import static com.github.hapily04.skriptminestom.util.MessageUtils.SKRIPT_MINI_M
 
 public class SkriptCommand extends Command {
 
+	/*
+		<yellow>disable <file> <base_grey>- Disable and unload an enabled script file.
+		<yellow>enable <file> <base_grey>- Enable and load a disabled script file.
+	 */
 	static final Component HELP_MESSAGE = SKRIPT_MINI_MESSAGE.deserialize("""
 		<skript_minestom_tag> <base_grey>Help
 		  <yellow>reload <all/folder/file/config> <base_grey>- Reload a scripts folder, script file, or the Skript config.
-		  <yellow>disable <file> <base_grey>- Disable and unload an enabled script file.
-		  <yellow>enable <file> <base_grey>- Enable and load a disabled script file.
 		  <yellow>info <base_grey>- Show addon information & server information.
 		  <yellow>help <base_grey>- Show this help message.""");
 
