f3d73b197ac7b26a59c0b19432a4c8ba7e2914fd
hapily <87914807+hapily04@users.noreply.github.com>
2026-08-01 01:24:29 -0500

    fix: EvtClick event values

--- minestom/src/main/java/ch/njol/skript/events/wrapper/PlayerBlockInteractWrapper.java
+++ minestom/src/main/java/ch/njol/skript/events/wrapper/PlayerBlockInteractWrapper.java
@@ -2,9 +2,25 @@ package ch.njol.skript.events.wrapper;
 
 import ch.njol.skript.events.wrapper.marker.BlockEventMarker;
 import ch.njol.skript.events.wrapper.marker.PlayerInstanceEventMarker;
+import ch.njol.skript.registrations.EventValues;
+import ch.njol.skript.util.Item;
+import ch.njol.skript.util.Slot;
+import net.minestom.server.entity.EquipmentSlot;
+import net.minestom.server.entity.Player;
+import net.minestom.server.entity.PlayerHand;
 import net.minestom.server.event.player.PlayerBlockInteractEvent;
+import org.skriptlang.skript.bukkit.lang.eventvalue.EventValue;
 
 public class PlayerBlockInteractWrapper extends EventWrapper<PlayerBlockInteractEvent> implements PlayerInstanceEventMarker, BlockEventMarker {
+
+	static {
+		EventValues.registerEventValue(EventValue.simple(PlayerBlockInteractWrapper.class, Slot.class, from -> {
+			PlayerBlockInteractEvent e = from.event;
+			Player player = e.getPlayer();
+			PlayerHand hand = e.getHand();
+			return new Slot(player.getItemInHand(hand), player, hand == PlayerHand.MAIN ? EquipmentSlot.MAIN_HAND : EquipmentSlot.OFF_HAND);
+		}));
+	}
 
 	public PlayerBlockInteractWrapper(PlayerBlockInteractEvent event) {
 		super(event);
--- minestom/src/main/java/ch/njol/skript/events/wrapper/PlayerEntityInteractWrapper.java
+++ minestom/src/main/java/ch/njol/skript/events/wrapper/PlayerEntityInteractWrapper.java
@@ -2,19 +2,26 @@ package ch.njol.skript.events.wrapper;
 
 import ch.njol.skript.events.wrapper.marker.PlayerInstanceEventMarker;
 import ch.njol.skript.registrations.EventValues;
+import ch.njol.skript.util.Slot;
 import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.EquipmentSlot;
+import net.minestom.server.entity.Player;
+import net.minestom.server.entity.PlayerHand;
+import net.minestom.server.event.player.PlayerBlockInteractEvent;
 import net.minestom.server.event.player.PlayerEntityInteractEvent;
 import org.skriptlang.skript.bukkit.lang.eventvalue.EventValue;
 
 public class PlayerEntityInteractWrapper extends EventWrapper<PlayerEntityInteractEvent> implements PlayerInstanceEventMarker {
 
 	static {
+		EventValues.registerEventValue(EventValue.simple(PlayerEntityInteractWrapper.class, Slot.class, from -> {
+			PlayerEntityInteractEvent e = from.event;
+			Player player = e.getPlayer();
+			PlayerHand hand = e.getHand();
+			return new Slot(player.getItemInHand(hand), player, hand == PlayerHand.MAIN ? EquipmentSlot.MAIN_HAND : EquipmentSlot.OFF_HAND);
+		}));
 		EventValues.registerEventValue(EventValue.builder(PlayerEntityInteractWrapper.class, Entity.class)
-			.patterns("attacker")
-			.getter(from -> from.getEvent().getPlayer())
-			.build());
-		EventValues.registerEventValue(EventValue.builder(PlayerEntityInteractWrapper.class, Entity.class)
-			.patterns("victim")
+			.patterns("clicked-entity")
 			.getter(from -> from.getEvent().getTarget())
 			.build());
 	}
