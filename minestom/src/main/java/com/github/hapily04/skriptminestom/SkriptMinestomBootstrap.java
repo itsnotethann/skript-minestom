73d1844be9bcb481d8f0fc320667d6aef9966637
Ethan <megaethanthekiller2613@gmail.com>
2026-08-03 16:02:04 -0400

    Route the connect event through SkriptMinestomBootstrap

--- minestom/src/main/java/com/github/hapily04/skriptminestom/SkriptMinestomBootstrap.java
+++ minestom/src/main/java/com/github/hapily04/skriptminestom/SkriptMinestomBootstrap.java
@@ -1,6 +1,9 @@
 package com.github.hapily04.skriptminestom;
 
+import ch.njol.skript.events.minestom.CustomConnectEvent;
+import net.minestom.server.entity.Player;
 import net.minestom.server.event.Event;
+import net.minestom.server.event.EventDispatcher;
 import net.minestom.server.event.EventNode;
 
 import java.net.URISyntaxException;
@@ -16,6 +19,12 @@ public final class SkriptMinestomBootstrap {
 
 	public static void bootEffectCommands(EventNode<Event> node) {
 		SkriptMinestom.initEffectCommands(node);
+	}
+
+	public static boolean fireConnectEvent(Player player) {
+		CustomConnectEvent connectEvent = new CustomConnectEvent(player);
+		EventDispatcher.call(connectEvent);
+		return !connectEvent.isKicked();
 	}
 
 }
